e86a50358db0573db3284725c627f3915de69b9e
King_8 <45991619+kart0050@users.noreply.github.com>
2026-09-21 14:56:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -176,7 +176,9 @@ const defaultCategories = [
       "Sana Palace RA",
       "Uranus Partyhouse",
       "E-Plaza AA",
+      "Elegance Brørup"
       "Babylon AA",
+      "Sociale Palace Silkeborg",
       "Herning Kovil",
       "Food & Drinks",
       "Sweets for children",
